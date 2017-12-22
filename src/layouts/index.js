e980b55057f1f53534660a44942e9d66155c954c
张哲铭 <1371955569@qq.com>
2017-12-22 20:38:06 +0800

    Initial commit

---
+++ src/layouts/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import '../assets/global.css'
+import styled from 'styled-components'
+
+export default ({ children }) => {
+	return (
+		<div>
+			<Header>
+			<Link to='/'>首页</Link>
+			</Header>
+			{children()}
+		</div>
+	)
+}
+
+const Header = styled.div`
+	line-height: 40px;
+	padding: 10px;
+	background: #00bcd4;
+	a {
+		color:white
+	}
+`
